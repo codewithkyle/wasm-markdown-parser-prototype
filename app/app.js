538ea395b669f41ac75e4e3c160d936b2f3147af
Kyle Andrews <kylea@page.works>
2020-03-04 07:06:12 -0500

    Updates UI

--- app/app.js
+++ app/app.js
@@ -1,11 +1,24 @@
 const rust = import('../pkg/rust_demo');
 import './vars.css';
+import './app.css';
 
 const preview = document.body.querySelector('#output');
 const input = document.body.querySelector('#input');
 
 rust.then(module => {
+    input.addEventListener('focus', () => {
+        if (input.value === 'Start typing to begin'){
+            input.value = '';
+        }
+    });
+    input.addEventListener('blur', () => {
+        if (input.value === ''){
+            input.value = 'Start typing to begin';
+        }
+    });
     input.addEventListener('keyup', () => {
-        preview.innerHTML = module.parse(input.value);
+        if (input.value !== 'Start typing to begin') {
+            preview.innerHTML = module.parse(input.value);
+        }
     });
 });
