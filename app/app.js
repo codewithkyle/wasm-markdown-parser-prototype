99624a2c6310560a60e70fc2872095707da12fcb
Kyle Andrews <kylea@page.works>
2020-03-04 07:46:54 -0500

    Adds HTML/Text output switcher functionality

--- app/app.js
+++ app/app.js
@@ -4,9 +4,12 @@ import './app.css';
 
 const preview = document.body.querySelector('#output');
 const input = document.body.querySelector('#input');
+const rawPreview = document.body.querySelector('#output-raw');
+let outputType = 'text';
 
 rust.then(module => {
     input.addEventListener('focus', () => {
+        settingsButton.classList.remove('is-open');
         if (input.value === 'Start typing to begin'){
             input.value = '';
         }
@@ -18,7 +21,56 @@ rust.then(module => {
     });
     input.addEventListener('keyup', () => {
         if (input.value !== 'Start typing to begin') {
-            preview.innerHTML = module.parse(input.value);
+            let result = module.parse(input.value);
+            switch (outputType){
+                case 'html':
+                    rawPreview.value = result;
+                    break;
+                default:
+                    preview.innerHTML = result;
+                    break;
+            }
         }
     });
 });
+
+preview.addEventListener('click', () => {
+    settingsButton.classList.remove('is-open');
+});
+rawPreview.addEventListener('click', () => {
+    settingsButton.classList.remove('is-open');
+});
+
+document.body.querySelectorAll('input[type="radio"]').forEach(input => {
+    input.addEventListener('change', (e) => {
+        outputType = e.currentTarget.value;
+        switch (outputType){
+            case 'html':
+                rawPreview.value = preview.innerHTML;
+                rawPreview.style.display = 'inline-block';
+                preview.style.display = 'none';
+                break;
+            default:
+                preview.innerHTML = rawPreview.value;
+                rawPreview.style.display = 'none';
+                preview.style.display = 'inline-block';
+                break;
+        }
+    });
+});
+
+document.body.addEventListener('keyup', (e) => {
+    if (e.key.toLowerCase() === 'escape'){
+        settingsButton.classList.remove('is-open');
+    }
+});
+
+const settingsButton = document.body.querySelector('#settings-button');
+settingsButton.addEventListener('click', (e) => {
+    if (settingsButton.classList.contains('is-open')){
+        settingsButton.classList.remove('is-open');
+    }else{
+        settingsButton.classList.add('is-open');
+    }
+});
+
