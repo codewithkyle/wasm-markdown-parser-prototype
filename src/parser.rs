03dd2184166b31e0184305e6a6a1b10f34052e36
Kyle Andrews <kylea@page.works>
2020-03-12 14:40:02 -0400

    Adds support for order/unordered lists & paragraph tags

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,16 @@
 struct Parser {
     pos: usize,
     input: String,
+    list_open: bool,
+    list_type: u8,
 }
 
 pub fn parse(source: String) -> String {
     Parser {
         pos: 0,
         input: source,
+        list_open: false,
+        list_type: 0,
     }
     .parse_lines()
 }
@@ -19,7 +23,10 @@ impl Parser {
             if self.end_of_line() {
                 break;
             }
-            result.push_str(&self.parse_line())
+            result.push_str(&self.parse_line());
+        }
+        if self.list_open {
+            result.push_str(&self.close_list())
         }
         result
     }
@@ -28,19 +35,63 @@ impl Parser {
         match self.next_char() {
             '#' => self.parse_title(),
             '-' => {
-                if char::is_whitespace(self.input[self.pos + 1..].chars().next().unwrap()) {
-                    self.parse_list()
-                } else {
-                    self.parse_text()
+                if !self.list_open {
+                    self.list_open = true;
+                    self.list_type = 1;
+                    return open_html_element("ul".to_string());
+                } else if self.list_open && self.list_type != 1 {
+                    return self.close_list();
                 }
+                return self.parse_list();
             }
-            _ => self.parse_text(),
+            x if x.is_numeric() => {
+                if !self.list_open {
+                    self.list_open = true;
+                    self.list_type = 2;
+                    return open_html_element("ol".to_string());
+                } else if self.list_open && self.list_type != 2 {
+                    return self.close_list();
+                }
+                return self.parse_ordered_list();
+            }
+            _ => {
+                if self.list_open {
+                    return self.close_list();
+                }
+                return self.parse_paragraph();
+            }
         }
+    }
+
+    fn parse_paragraph(&mut self) -> String {
+        let result = self.consume_while(|c| !is_newline(c));
+        create_html_element("p".to_string(), result)
+    }
+
+    fn parse_ordered_list(&mut self) -> String {
+        self.consume_char();
+        self.consume_while(|c| c != '.');
+        self.consume_char();
+        if self.next_char().is_whitespace() {
+            self.consume_whitespace();
+        }
+        let text = self.parse_text();
+        create_html_element("li".to_string(), text)
+    }
+
+    fn close_list(&mut self) -> String {
+        self.list_open = false;
+        match self.list_type {
+            1 => return close_html_element("ul".to_string()),
+            _ => return close_html_element("ol".to_string()),
+        };
     }
 
     fn parse_list(&mut self) -> String {
         self.consume_char();
-        self.consume_whitespace();
+        if self.next_char().is_whitespace() {
+            self.consume_whitespace();
+        }
         let text = self.parse_text();
         create_html_element("li".to_string(), text)
     }
@@ -97,6 +148,18 @@ impl Parser {
 fn create_html_element(tag_name: String, text: String) -> String {
     format!("<{}>{}</{}>\n", tag_name, text, tag_name)
 }
+
+fn open_html_element(tag_name: String) -> String {
+    format!("<{}>\n", tag_name)
+}
+
+fn close_html_element(tag_name: String) -> String {
+    format!("</{}>\n", tag_name)
+}
+
+// fn create_self_closing_html_element(tag_name: String) -> String {
+//     format!("<{}/>\n", tag_name)
+// }
 
 fn is_newline(c: char) -> bool {
     c == '\n'
